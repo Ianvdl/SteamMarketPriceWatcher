3073c0f82d521f1d552a0c2a636ad67e1014a978
jvdlinde1 <jvdlinde1@gmail.com>
2015-07-23 21:28:12 +0200

    Initial Commit
    First commit of the Steam Marketplace Price Watcher

---
+++ steam_price_watcher.py
@@ -0,0 +1,37 @@
+#J. van der Linde
+#Steam Marketplace Price Watcher
+#Last updated 2015-07-23
+
+import urllib
+import re
+import time
+import sched
+import sys
+
+#==============================================================
+#As a user, the lines below are all you need to change
+appid = '570'                   #570 is DOTA2
+item_name = 'Faceless%20Rex'    #Item name as found in web URL
+ideal_price = 20                #Ideal price in USD
+check_timeout = 10              #Delay between checks in seconds
+#Don't mess with anything below this line unless you're a programmer
+#==============================================================
+
+def getPrices():
+    url = 'http://steamcommunity.com/market/priceoverview/?currency=1&appid=' + appid + '&market_hash_name=' + item_name
+    regex_pattern = re.compile('\"lowest_price\":.*?([0-9]+\.[0-9]*)', re.IGNORECASE|re.DOTALL)
+    page = urllib.urlopen(url).read()
+
+    regex_matches = regex_pattern.findall(page)
+
+    if (float(regex_matches[0]) <= ideal_price):
+        print "Lowest Price: USD$ " + regex_matches[0] + "     <===== Lower than your ideal price! Buy!"
+    else:
+        print "Lowest Price: USD$ " + regex_matches[0]
+    sys.stdout.flush()
+
+    s = sched.scheduler(time.time, time.sleep)
+    s.enter(10, check_timeout, getPrices, ())
+    s.run()
+
+getPrices()
