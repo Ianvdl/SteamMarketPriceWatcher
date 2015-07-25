5aadf15816c83564fa4ecb1923e552879da3d315
jvdlinde1 <jvdlinde1@gmail.com>
2015-07-25 10:43:52 +0200

    Added record low
    Added lowest recorded price since script started

--- steam_price_watcher.py
+++ steam_price_watcher.py
@@ -18,22 +18,25 @@ check_timeout = 10              #Delay between checks in seconds
 #Don't mess with anything below this line unless you're a programmer
 #==============================================================
 
+lowest_recorded = 999999999
+
 def getPrices():
+    global lowest_recorded
     url = 'http://steamcommunity.com/market/priceoverview/?currency=1&appid=' + appid + '&market_hash_name=' + item_name
     regex_pattern = re.compile('\"lowest_price\":.*?([0-9]+\.[0-9]*)', re.IGNORECASE|re.DOTALL)
     page = urllib.urlopen(url).read()
 
     regex_matches = regex_pattern.findall(page)
+    price = float(regex_matches[0])
+    if price < lowest_recorded:
+        lowest_recorded = price
 
-    if (float(regex_matches[0]) <= ideal_price):
-        print "Lowest Price: USD$ " + regex_matches[0] + "     <===== Lower than your ideal price! Buy!"
+    if (price <= ideal_price):
+        print "Lowest Price: USD$ " + str(price) + "\t<===== Lower than your ideal price! Buy!"
     else:
-        print "Lowest Price: USD$ " + regex_matches[0] + "            " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print "Lowest Price: USD$ " + str(price) + "\t" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\tRecord Low: USD$ " + str(lowest_recorded)
     sys.stdout.flush()
 
-    #s = sched.scheduler(time.time, time.sleep)
-    #s.enter(10, check_timeout, getPrices, ())
-    #s.run()
     time.sleep(check_timeout)
     getPrices()
 
